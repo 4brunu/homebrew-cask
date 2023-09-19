83739f6d524410d94b074719c4e637e6efc0de98
Bruno Coelho <4brunu@users.noreply.github.com>
2023-09-19 18:42:26 +0100

    Update zulu.rb to jdk 21 lts

--- Casks/z/zulu.rb
+++ Casks/z/zulu.rb
@@ -1,10 +1,10 @@
-cask "zulu" do
+cask "zulu21" do
   arch arm: "aarch64", intel: "x64"
   choice = on_arch_conditional arm: "arm", intel: "x86"
 
-  version "20.0.2,20.32.11_1-ca"
-  sha256 arm:   "c5c196a0a25eca25a22edee325ff8ee2cd96aa9bfdee0ca3554dfbfd31dba8df",
-         intel: "6f02981801832230edd0a0091b8de10873d3271cd7cc4d7a9ed439b046fa4b1f"
+  version "21.0.0,21.28.85-ca"
+  sha256 arm:   "d9ce2ff2be7decf614bf77b4fc5cf119186effc82fff213e14a45bbc33793af4",
+         intel: "d8fc881a204400633189491f411886ee9338d6081767c1efba09b700df92fa0b"
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.dmg",
       referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
@@ -14,19 +14,17 @@ cask "zulu" do
 
   livecheck do
     url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&bundle_type=jdk&javafx=false&ext=dmg&os=macos&arch=#{choice}"
-    strategy :page_match do |page|
-      match = page.match(/zulu(\d+(?:[._]\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macosx_#{arch}\.dmg/i)
-      next if match.blank?
-
-      "#{match[2]},#{match[1]}"
+    regex(/zulu(\d+(?:[._]\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macosx_#{arch}\.dmg/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
     end
   end
 
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :big_sur"
 
   pkg "Double-Click to Install Azul Zulu JDK #{version.major}.pkg"
 
   uninstall pkgutil: "com.azulsystems.zulu.#{version.major}"
 
-  # No zap stanza required
+  zap trash: "~/Library/Saved Application State/com.azul.zulu.#{version.major}*.java.savedState"
 end
